35b5d06503e40738339830124d11105574c6a7ec
Ariel Lopes <ariellopes888@gmail.com>
2022-07-19 16:52:49 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/AlunoController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PoCWebAPI.Models;
+
+namespace PoCWebAPI.Controllers
+
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private EscolaContext escola = new EscolaContext();
+        
+
+        [HttpGet]
+        public Aluno[] AcessarAluno()
+        {
+            return escola.Alunos.ToArray();
+        }
+
+        [HttpGet("{Id}")]
+        public Aluno AcessarAlunoId(int Id)
+        {
+            Aluno Resultado = new Aluno();
+            foreach (Aluno aluno in escola.Alunos)
+            {
+                if (aluno.Id == Id)
+                {
+                    Resultado = aluno;
+                }
+            }
+            return Resultado;
+        }
+
+        [HttpPost]
+        public Aluno AdicionarAlunoId(Aluno novoAluno)
+        {
+            escola.Alunos.Add(novoAluno);
+            escola.SaveChanges();
+            return novoAluno;
+        }
+
+
+        [HttpDelete("{Id}")]
+        public Aluno DeletarAlunoId(int Id)
+        {
+            Aluno AlunoDeletado = new Aluno();
+
+            foreach(Aluno aluno in escola.Alunos)
+            {
+                if(aluno.Id == Id)
+                {
+                    AlunoDeletado = aluno;
+                    escola.Alunos.Remove(AlunoDeletado);
+                    escola.SaveChanges();
+                }
+
+            }
+            return AlunoDeletado;
+        }
+
+        
+        
+
+    }
+
+}
